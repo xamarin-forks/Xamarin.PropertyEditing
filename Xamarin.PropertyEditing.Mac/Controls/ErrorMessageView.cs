7a994ae69b1e4e8ad278549547b8dae8c4899782
Dominique Louis <savagesoftware@gmail.com>
2018-03-21 11:36:08 +0100

    Refactored PopOver because we'll need these as the base for CustomExpression and RequestResource PRs

--- Xamarin.PropertyEditing.Mac/Controls/ErrorMessageView.cs
+++ Xamarin.PropertyEditing.Mac/Controls/ErrorMessageView.cs
@@ -1,33 +1,34 @@
 ﻿using System;
 using System.Collections;
 using AppKit;
+using CoreGraphics;
 
 namespace Xamarin.PropertyEditing.Mac
 {
-	internal class ErrorMessageView : BaseEditorControl
+	internal class ErrorMessageView : BasePopOverControl
 	{
-		NSTextField ErrorMessages;
-
-		public ErrorMessageView (IEnumerable errors)
+		NSTextField ErrorMessages; public ErrorMessageView (IEnumerable errors) : base ("Errors", "action-warning-16")
 		{
 			if (errors == null)
-				throw new ArgumentNullException ("errors");
+				throw new ArgumentNullException (nameof (errors));
+
+			Frame = new CGRect (CGPoint.Empty, new CGSize (240, 480));
 
 			ErrorMessages = new NSTextField {
-				TranslatesAutoresizingMaskIntoConstraints = false,
 				BackgroundColor = NSColor.Clear,
 				Editable = false,
+				TranslatesAutoresizingMaskIntoConstraints = false,
 			};
 
 			foreach (var error in errors) {
 				ErrorMessages.StringValue += error + "\n";
 			}
 
-			AddSubview (ErrorMessages);
-
-			this.DoConstraints (new[] {
-				ErrorMessages.ConstraintTo (this, (s, c) => s.Width == c.Width - 5),
-				ErrorMessages.ConstraintTo (this, (s, c) => s.Height == c.Height - 5),
+			AddSubview (ErrorMessages); this.DoConstraints (new[] {
+				ErrorMessages.ConstraintTo (this, (s, c) => s.Top == c.Top + 35),
+				ErrorMessages.ConstraintTo (this, (s, c) => s.Left == c.Left + 5),
+				ErrorMessages.ConstraintTo (this, (s, c) => s.Width == c.Width - 10),
+				ErrorMessages.ConstraintTo (this, (s, c) => s.Height == c.Height - 40),
 			});
 		}
 	}
